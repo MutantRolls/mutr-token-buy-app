e10afa44e7e499e8b780ac959cc8dc02b1f32fdc
MutantRolls <info@mutantrolls.com>
2026-03-04 17:18:55 +0200

    Update DepositPage.tsx

--- app/components/DepositPage.tsx
+++ app/components/DepositPage.tsx
@@ -7,7 +7,7 @@ import Head from 'next/head';
 
 // USDC Token Mint on Devnet
 const USDC_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
-const RECEIVER_WALLET = new PublicKey("2u9yhxApCZWwAy38qtkkJNVNmZxqE4qdeahZM8ANWk23"); // Replace with your wallet address
+const RECEIVER_WALLET = new PublicKey("HQ14VLzczC4UR1HRpVKgqzFp9jZNZigADCNbiKxH6t3q"); // Replace with your wallet address
 const MUTR_EXCHANGE_RATE_SOL = 380000; // 1 SOL = 100 MUTR
 const MUTR_EXCHANGE_RATE_USDC = 5000; // 1 USDC = 2 MUTR
 
